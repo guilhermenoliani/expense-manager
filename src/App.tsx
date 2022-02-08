d35d5dc5821421abb27d6a03bb368bae4db64fd8
Guilherme Oliani <guilhermenoliani@gmail.com>
2022-02-08 12:11:45 -0300

    Criando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import {Item} from './types/Item';
+import {Category} from './types/Category';
+import {categories} from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
+
 
 const App = () => {
+
+  const [list, setList]= useState(items);
+  const [ filteredList, setFilteredList ] = useState<Item[]>([])
+  const [ currentMonth, setCurrentMounth ] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+      setFilteredList(filterListByMonth(list, currentMonth) );
+  },[list, currentMonth])
+
+
   return(
     <C.Container>
       <C.Header>
@@ -14,7 +32,7 @@ const App = () => {
       {/*Area de inserção*/}
 
 
-      {/*Tabela de itens*/}
+      <TableArea/>
 
 
 
---
+++ src/components/TableArea/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Table = styled.table `
+`;
+
+export const TableHeadColumn = styled.th`
+`;
